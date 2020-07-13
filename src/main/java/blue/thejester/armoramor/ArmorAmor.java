4e33ba2f3f188c1ef51793df956e16502c04e637
Jacob Inkrote <jaketheultimate@gmail.com>
2020-07-13 09:18:48 -0400

    Change mod name from copied files

--- src/main/java/blue/thejester/armoramor/ArmorAmor.java
+++ src/main/java/blue/thejester/armoramor/ArmorAmor.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class ArmorAmor
 {
     public static final String MODID = "armoramor";
-    public static final String NAME = "Botany Booster";
+    public static final String NAME = "Armor Amor";
     public static final String VERSION = "1.0";
 
     public static Logger logger;
